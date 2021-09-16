81828943cc4ff4599a48b921fb3d9b66650bd3a2
kimjinsoo <1004kjs_@naver.com>
2021-09-16 22:01:38 +0900

    create 단위테스트 학습

--- controller/product.js
+++ controller/product.js
@@ -1,3 +1,5 @@
-exports.hello = (req, res) => {
-    res.send("안녕하세요")
+const productModel = require('../models/Product')
+
+exports.createProduct = (req, res, next) => {
+    productModel.create(req.body)
 }
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,9 +1,22 @@
-describe("Calculation", () => {
-    test('two plus two is four', () => {
-        expect(2 + 2).toBe(4);
+const productController = require('../../controller/product');
+const productModel = require('../../models/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
+
+
+productModel.create = jest.fn()
+
+describe("Product Controller Create", () => {
+    test("should have a createProduct function", () => {
+        expect(typeof productController.createProduct).toBe("function");
     })
 
-    test('two plus two is not five', () => {
-        expect(2 + 2).not.toBe(5);
+    test("should call ProductModel.create", () => {
+        let req = httpMocks.createRequest();
+        let res = httpMocks.createResponse();
+        let next = null;
+        req.body = newProduct
+        productController.createProduct(req, res, next);
+        expect(productModel.create).toBeCalledWith(newProduct);
     })
 })
